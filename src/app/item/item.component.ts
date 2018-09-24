540c85cdc34930288978d32a2d3733733256057b
Gulyas Zsolt <zsolt.gulyas@outlook.com>
2018-09-24 17:29:36 +0200

    Add services

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { StarWarsService } from '../star-wars.service';
 
 @Component({
   selector: 'app-item',
@@ -7,16 +8,19 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
   @Input() character;
-  @Output() sideAssigned = new EventEmitter<{name: string, side: string}>();
+  swService: StarWarsService;
 
-  constructor() { }
+  constructor(swService: StarWarsService) {
+    this.swService = swService;
+  }
 
   ngOnInit() {
   }
 
   onAssign(side) {
     // this.character.side = side;
-    this.sideAssigned.emit({name: this.character.name, side: side});
+    // this.sideAssigned.emit({name: this.character.name, side: side});
+    this.swService.onSideChosen({name: this.character.name, side: side});
   }
 
 }
--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StarWarsService } from '../star-wars.service';
 
 @Component({
   selector: 'app-tabs',
@@ -6,14 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.component.css']
 })
 export class TabsComponent implements OnInit {
-  characters = [
-    { name: 'Luke Skywalker', side: ''},
-    { name: 'Darth Vader', side: ''},
-  ];
-
+  characters = [];
   chosenList = 'all';
+  swService: StarWarsService;
 
-  constructor() { }
+  constructor(swService: StarWarsService) {
+    this.swService = swService;
+  }
 
   ngOnInit() {
   }
@@ -23,19 +23,10 @@ export class TabsComponent implements OnInit {
   }
 
   getCharacters(){
-    if (this.chosenList === 'all') {
-      return this.characters.slice();
-    }
-    return this.characters.filter( (char) => {
-      return char.side === this.chosenList;
-    })
+    this.characters = this.swService.getCharacters(this.chosenList);
+    return this.characters;
   }
 
-  onSideChosen(charInfo) {
-    const pos = this.characters.findIndex((char) => {
-      return char.name === charInfo.name;
-    })
-    this.characters[pos].side = charInfo.side;
-  }
+
 
 }
