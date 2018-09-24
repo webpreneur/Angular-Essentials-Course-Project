dc290062b4b89d1ab0f586bebb1e5f52a64eea6f
Gulyás Zsolt <zsolt.gulyas@moresimp.com>
2018-09-24 08:46:22 +0200

    Finishing preparation for services integration

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -7,10 +7,15 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
   @Input() characters;
+  @Output() sideAssigned = new EventEmitter<{name: string, side: string}>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSideAssigned(charInfo) {
+    this.sideAssigned.emit(charInfo);
   }
 
 }
--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -31,4 +31,11 @@ export class TabsComponent implements OnInit {
     })
   }
 
+  onSideChosen(charInfo) {
+    const pos = this.characters.findIndex((char) => {
+      return char.name === charInfo.name;
+    })
+    this.characters[pos].side = charInfo.side;
+  }
+
 }
